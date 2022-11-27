8953e92aa1e118b9b172e92949878eb29c92f64e
Fabricyo Ribeiro <fabricyosilva600@gmail.com>
2022-11-26 23:15:44 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import Glide from "@glidejs/glide";
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+}).reveal(`
+  #nav,
+  #main,
+  #style,
+  #catalog,
+  #about
+`);
 
-new Glide('.glide').mount()
+ScrollReveal({
+  origin: 'right',
+  distance: '90%',
+  duration: 2000,
+}).reveal(`
+  #main #first, 
+  #main #second,
 
+  #about #first,
+  #about #second
+`);
